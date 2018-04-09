b3b184789ddafc9d9a7a9632a6dd8c590720713a
Lydmilamihaleva <Lydmilamihaleva@mail.ru>
2018-04-09 16:35:32 +0500

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,1 +1,13 @@
+#coding: utf-8
+
+from flask import Flask, request
+from json import json
+app = Flask(__name__)
+
+@app.route("/json.json")
+def get_tasks():
+    return ""
+if __name__ == "__main__":
+    app.run()
+
 
